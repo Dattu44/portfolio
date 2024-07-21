fa06af35d3bcd5ed782df1beabc4c2b911b5c3f1
Dattu44 <addankijnanadath@gmail.com>
2024-07-21 12:41:42 +0530

    Add files via upload

---
+++ index_function.js
@@ -0,0 +1,26 @@
+function greet(){
+    console.log("Hello, World!")
+}
+greet()
+
+//function with parameters
+function addNumbers(a, b){
+    console.log("sum",a+b)
+}
+addNumbers(5, 20)
+
+//function with Return Statements
+function multiply(x, y){
+    return x*y
+}
+let result=multiply(10, 20)
+console.log("multiply",result)
+
+//Combining Functions
+function calculateRectangleArea(length, width){
+    return multiply(length, width)
+    
+}
+let area=calculateRectangleArea(10, 30)
+console.log("area",area)
+
